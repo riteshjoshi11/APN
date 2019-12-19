e76092ca37861b2f0f46bed201bff6c94462d58e
RJ5029318 <Amex@97691>
2019-12-19 15:55:47 +0530

    Try commit

--- Hisab/src/main/java/com/ANP/repository/CustomerDao.java
+++ Hisab/src/main/java/com/ANP/repository/CustomerDao.java
@@ -23,17 +23,17 @@ public class CustomerDao {
     public List<Customer> getCustomer(){
 
         List <Customer> customers =new ArrayList<Customer>();
-        Customer cu1=new Customer();
-        cu1.setName("Ritesh");
-        cu1.setCity(1);
-        cu1.setOrgId(1);
-
-        Customer cu2=new Customer();
-        cu2.setName("Nitesh");
-        cu2.setCity(2);
-        cu2.setOrgId(2);
-        customers.add(cu1);
-        customers.add(cu2);
+//        Customer cu1=new Customer();
+//        cu1.setName("Ritesh");
+//        cu1.setCity(1);
+//        cu1.setOrgId(1);
+//
+//        Customer cu2=new Customer();
+//        cu2.setName("Nitesh");
+//        cu2.setCity(2);
+//        cu2.setOrgId(2);
+//        customers.add(cu1);
+//        customers.add(cu2);
         return customers;
 
     }
