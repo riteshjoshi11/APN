0b8cc740127cb55e36714d57315c53494b4003da
Ritesh Joshi <riteshjoshi11@gmail.com>
2020-05-02 16:28:10 +0530

    hide unused parameter in swagger

--- Hisab/src/main/java/com/ANP/repository/DashboardDAO.java
+++ Hisab/src/main/java/com/ANP/repository/DashboardDAO.java
@@ -4,7 +4,9 @@ import com.ANP.bean.CalculationTrackerBean;
 import com.ANP.bean.DashboardBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class DashboardDAO {
 
     @Autowired
