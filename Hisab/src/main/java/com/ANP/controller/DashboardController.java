4a32d21bdd221f00fdd2174c06dd8a416e3b2378
Nitesh Yadav <nitesh.yadav@idmworks.com>
2020-05-27 19:49:46 +0530

    Fixed issue related to running buildDashboard: need to work more on it.

--- Hisab/src/main/java/com/ANP/controller/DashboardController.java
+++ Hisab/src/main/java/com/ANP/controller/DashboardController.java
@@ -3,10 +3,7 @@ package com.ANP.controller;
 import com.ANP.bean.DashboardBean;
 import com.ANP.repository.DashboardDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/dashboard")
@@ -16,7 +13,7 @@ public class DashboardController {
     DashboardDAO dashboardDAO ;
 
     @PostMapping(path = "/buildDashboard", produces = "application/json")
-    public DashboardBean buildDashboard(@RequestBody long orgId) {
+    public DashboardBean buildDashboard(@RequestParam long orgId) {
         return dashboardDAO.prepareDashBoard(orgId);
     }
 }
