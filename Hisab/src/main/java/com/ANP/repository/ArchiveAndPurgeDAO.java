edb399a0bcbd304d14fea1a9e062dd2323d0d0ae
Paras Kavdikar <paraskavdikar@gmail.com>
2020-06-25 17:35:50 +0530

    ArchiveAndPurgeDAO completed and added the latest patch.

--- Hisab/src/main/java/com/ANP/repository/ArchiveAndPurgeDAO.java
+++ Hisab/src/main/java/com/ANP/repository/ArchiveAndPurgeDAO.java
@@ -1,6 +1,8 @@
 package com.ANP.repository;
 
+import com.ANP.util.CustomAppException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -8,33 +10,52 @@ import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Repository
 public class ArchiveAndPurgeDAO {
 
     @Autowired
+    SystemUtilityDAO systemUtilityDAO;
+
+    @Autowired
     JdbcTemplate jdbcTemplate;
     @Transactional(rollbackFor = Exception.class)
-    public void actualDeletion(Map<String,String> commaSeperatedList , int noOfDaysAfterDelete)
+    public void actualDeletion()
     {
-        String list = commaSeperatedList.get("archiveandpurgetablelist");
-        List<String> result = Arrays.asList(list.split("\\s*,\\s*"));
-        SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("PurgeProcess_Procedure");
-        Map<String, Object> inParamMap = new HashMap<String, Object>();
-        for(Object o : result)
+        Map<String,String> systemConfigMap ;
+        int noOfDaysAfterDelete;
+        systemConfigMap = systemUtilityDAO.getSystemConfigurationMap();
+        String commaSeperatedArchiveList = systemConfigMap.get("archiveandpurgetablelist");
+        String deleteDays = systemConfigMap.get("daysafterdeletelist");
+        int noDeleteDays;
+        List<String> resultArchiveTableList = Arrays.asList(commaSeperatedArchiveList.split("\\s*,\\s*"));
+        try {
+            noDeleteDays = Integer.parseInt(deleteDays);
+        }
+        catch(Exception e)
         {
-            if(!o.equals("")) {
-                System.out.println("no fo days" + noOfDaysAfterDelete);
-                System.out.println("object" + o);
-                inParamMap.put("noofdaysafterdeletion", noOfDaysAfterDelete);
-                inParamMap.put("purgearchivetablename", o);
+            throw new CustomAppException("delete Days invalid","SERVER.SYSTEM_CONFIG.INVALIDVALUE", HttpStatus.EXPECTATION_FAILED);
+        }
+        if(noDeleteDays < 7){
+            noDeleteDays = 7;
+        }
+        SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("PurgeProcess_Procedure");
+        Map<String, Object> inParamMap = new HashMap<>();
+
+        Iterator<String> it1 = resultArchiveTableList.iterator();
+
+        for(Object listIterator : resultArchiveTableList)
+        {
+            if(!listIterator.equals("")) {
+
+                System.out.println("no fo days" + noDeleteDays);
+                System.out.println("object" + listIterator);
+                inParamMap.put("noofdaysafterdeletion", noDeleteDays);
+                inParamMap.put("purgearchivetablename", listIterator);
                 SqlParameterSource in = new MapSqlParameterSource(inParamMap);
                 Map<String, Object> simpleJdbcCallResult = simpleJdbcCall.execute(in);
-                System.out.println(simpleJdbcCallResult);
+                System.out.println("result = "+ simpleJdbcCallResult);
             }
         }
     }
---
+++ db/systemconfiguration.sql
@@ -0,0 +1,3 @@
+use `antrackerdb``;
+insert into `systemconfigurations`(`key`) values('ArchiveAndPurgeTableList');
+insert into `systemconfigurations`(`key`) values('daysafterdeletelist');
