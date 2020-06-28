324fcead408e014828e55148dad37ca682eabff0
Paras Kavdikar <paraskavdikar@gmail.com>
2020-06-28 16:52:43 +0530

    EmployeeType table created and values inserted in it.

---
+++ db/patch_28June.sql
@@ -0,0 +1,12 @@
+use antrackerdb;
+SET SQL_SAFE_UPDATES = 0;
+DROP TABLE IF EXISTS employeetype;
+
+CREATE TABLE `employeetype` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(75) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO employeetype VALUES (5,'accountant'),(4,'labour'),(1,'owner'),(2,'partner'),(3,'sales');
