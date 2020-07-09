687f9bd7f6c63afc62d48946e134085bd89b5cc9
Paras Kavdikar <paraskavdikar@gmail.com>
2020-07-09 15:37:03 +0530

    added attachment feature in email.

--- Hisab/src/main/java/com/ANP/util/EmailUtil.java
+++ Hisab/src/main/java/com/ANP/util/EmailUtil.java
@@ -1,14 +1,21 @@
 package com.ANP.util;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.mail.Session;
 import javax.mail.Transport;
 
+
 public class EmailUtil {
 
-    public void sendEmail(String recipient, String sender, String senderPassword, String host) {
+    public void sendEmail(String recipient, String sender, String senderPassword, String host, String pathname) {
 
         Properties properties = System.getProperties();
 
@@ -32,20 +39,29 @@ public class EmailUtil {
         {
             // MimeMessage object.
             MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            message.setSubject("Sorry for disturbing at this hour");
+            message.setText("Did I mistakenly set that as subject? Oh yes I did Well I am sorry about that too");
+
+            MimeBodyPart textBodyPart = new MimeBodyPart();
+
+            Multipart multipart = new MimeMultipart();
+
 
             // Set From Field: adding senders email to from field.
             message.setFrom(new InternetAddress(sender));
 
-            // Set To Field: adding recipient's email to from field.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
 
-            // Set Subject: subject of the email
-            message.setSubject("Sorry for disturbing at this hour");
 
-            // set body of the email.
-            message.setText("Did I mistakenly set that as subject? Oh yes I did Well I am sorry about that too");
+            Path path = Paths.get(pathname);
+            String fileName = path.getFileName().toString();
+            DataSource source = new FileDataSource(pathname);
+            textBodyPart.setDataHandler(new DataHandler(source));
+            textBodyPart.setFileName(fileName);
+            multipart.addBodyPart(textBodyPart);
 
-            // Send email.
+            message.setContent(multipart);
+
             Transport.send(message);
             System.out.println("Mail successfully sent");
         }
