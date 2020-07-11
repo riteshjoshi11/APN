01617709c46af3e0a3a7bebec8bc0c84446fc7af
Nitesh Yadav <nitesh.yadav@idmworks.com>
2020-07-11 18:37:32 +0530

    Fixed: Email utility method made robust (all null checks and required checks are in place and properties are set what is optional vs required)

--- Hisab/src/main/java/com/ANP/util/EmailUtil.java
+++ Hisab/src/main/java/com/ANP/util/EmailUtil.java
@@ -14,97 +14,121 @@ import javax.mail.Transport;
 
 
 public class EmailUtil {
-
-
+    /*
+     * This method is common util method for sending email
+     * It reads the host / port / fromAddress information from the systemconfiguration
+     * host and from address is mandatory
+     * Optional:
+     * * default port 25 (if you don't specify as part of SystemConf
+     * If your SMTP gateway supports SSL then - You can enable SSL/TLS using: EMAIL.UTILITY.SSLEnabled=true
+     * If your SMTP gateway password protected: EMAIL.UTILITY.PASSWORD=<Actual Password>
+     *
+     */
     public void sendEmail(String body, String[] toAddresses, String[] ccAdresses,
                           String subject, List<String> attachmentList) throws Exception {
 
-        if(attachmentList.isEmpty()) {
-            throw new CustomAppException("No Attachments were found","SERVER.SEND_EMAIL.DOESNOTEXIST", HttpStatus.EXPECTATION_FAILED);
-        }
-
-        if(ANPUtils.isNullOrEmpty(toAddresses[0]) && ANPUtils.isNullOrEmpty(ccAdresses[0])){
-            throw new CustomAppException("Addresses not found","SERVER.SEND_EMAIL.DOESNOTEXIST", HttpStatus.EXPECTATION_FAILED);
+        //atleast one field from toAddresses or ccAddresses must be present
+        if (toAddresses == null || (ANPUtils.isNullOrEmpty(toAddresses[0]) && (ccAdresses == null || ANPUtils.isNullOrEmpty(ccAdresses[0])))) {
+            throw new CustomAppException("No ToAddress or CCAddress present for sending Email", "SERVER.SEND_EMAIL.TOORCC_DOESNOTEXIST", HttpStatus.EXPECTATION_FAILED);
         }
 
         InternetAddress[] toAddressFormatted = new InternetAddress[toAddresses.length];
-        for( int i=0; i < toAddressFormatted.length; i++ ) {
+        for (int i = 0; i < toAddressFormatted.length; i++) {
             toAddressFormatted[i] = new InternetAddress(toAddresses[i]);
         }
 
         InternetAddress[] ccAddressFormatted = new InternetAddress[ccAdresses.length];
-        for(int i = 0 ; i< ccAddressFormatted.length;i++){
+        for (int i = 0; i < ccAddressFormatted.length; i++) {
             ccAddressFormatted[i] = new InternetAddress(ccAdresses[i]);
         }
+
         Properties properties = System.getProperties();
 
+
         //Getting the SystemConfigurations settings into a map
-        Map<String,String> systemConfigMap ;
+        Map<String, String> systemConfigMap;
         SystemConfigurationReaderDAO systemConfigurationReaderDAO = new SystemConfigurationReaderDAO();
         systemConfigMap = systemConfigurationReaderDAO.getSystemConfigurationMap();
 
         String host = systemConfigMap.get("EMAIL.UTILITY.HOST");
         String port = systemConfigMap.get("EMAIL.UTILITY.PORT");
+
+        if (ANPUtils.isNullOrEmpty(host)) {
+            throw new CustomAppException("Host cannot be null", "SERVER.SEND_EMAIL.DOESNOTEXIST", HttpStatus.EXPECTATION_FAILED);
+        }
+
+        // Setting up mail server host
+        properties.put("mail.smtp.host", host);
+
+        if (!ANPUtils.isNullOrEmpty(port) && (Integer.parseInt(port)) > 0) {
+            properties.put("mail.smtp.port", port);
+        } else {
+            properties.put("mail.smtp.port", 25);
+        }
+
+        String sslEnabled = systemConfigMap.get("EMAIL.UTILITY.SSLEnabled");
+
+        String trust = null;
+        if (!ANPUtils.isNullOrEmpty(sslEnabled) && sslEnabled.equalsIgnoreCase("true")) {
+            trust = systemConfigMap.get("EMAIL.UTILITY.TRUST");
+            properties.put("mail.smtp.starttls.enable", "true");
+            properties.put("mail.smtp.ssl.trust", trust);
+
+        }
+
         String fromEmailPassword = systemConfigMap.get("EMAIL.UTILITY.PASSWORD");
         String fromEmailAddress = systemConfigMap.get("EMAIL.UTILITY.FROMEMAILADDRESS");
-        String trust = systemConfigMap.get("EMAIL.UTILITY.TRUST");
 
-        if(ANPUtils.isNullOrEmpty(host)) {
-            throw new CustomAppException("Host cannot be null","SERVER.SEND_EMAIL.DOESNOTEXIST", HttpStatus.EXPECTATION_FAILED);
+        Session session = null;
+
+        if (!ANPUtils.isNullOrEmpty(fromEmailPassword)) {
+            properties.put("mail.smtp.auth", true);
+            session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(
+                            fromEmailAddress, fromEmailPassword);// Specify the Username and the PassWord
+                }
+            });
+        } else {
+            session = Session.getDefaultInstance(properties);
         }
-        else if(ANPUtils.isNullOrEmpty(port)){
-            throw new CustomAppException("Port cannot be null","SERVER.SEND_EMAIL.DOESNOTEXIST", HttpStatus.EXPECTATION_FAILED);
-        }
-
-        // Setting up mail server
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port",port);
-        properties.put("mail.smtp.starttls.enable","true");
-        properties.put("mail.smtp.ssl.trust", trust);
-        properties.put("mail.smtp.auth", true);
-
-
 
         // creating session object to get properties
-        Session session = Session.getDefaultInstance(properties,new javax.mail.Authenticator(){
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(
-                        fromEmailAddress, fromEmailPassword);// Specify the Username and the PassWord
-            }
-        });
-        try
-        {
+
+        try {
             // MimeMessage object.
             MimeMessage message = new MimeMessage(session);
             message.setRecipients(Message.RecipientType.TO, toAddressFormatted);
-            message.addRecipients(Message.RecipientType.CC,ccAddressFormatted);
+            message.addRecipients(Message.RecipientType.CC, ccAddressFormatted);
             message.setSubject(subject);
-
-
 
             // Set From Field: adding senders email to from field.
             message.setFrom(new InternetAddress(fromEmailAddress));
 
-            Multipart multipart = new MimeMultipart();
-            for(String str : attachmentList) {
-                MimeBodyPart messageBodyPartForAttachments = new MimeBodyPart();
-                DataSource source = new FileDataSource(str);
-                messageBodyPartForAttachments.setDataHandler(new DataHandler(source));
-                messageBodyPartForAttachments.setFileName(source.getName());
-                multipart.addBodyPart(messageBodyPartForAttachments);
+            Multipart multipart = null;
+            if (attachmentList != null && attachmentList.size() > 0) {
+                multipart = new MimeMultipart();
+                for (String str : attachmentList) {
+                    MimeBodyPart messageBodyPartForAttachments = new MimeBodyPart();
+                    DataSource source = new FileDataSource(str);
+                    messageBodyPartForAttachments.setDataHandler(new DataHandler(source));
+                    messageBodyPartForAttachments.setFileName(source.getName());
+                    multipart.addBodyPart(messageBodyPartForAttachments);
+                }
             }
 
             MimeBodyPart messageBodyPartForText = new MimeBodyPart();
             messageBodyPartForText.setText(body);
-            multipart.addBodyPart(messageBodyPartForText);
 
-            message.setContent(multipart);
+            if (multipart != null) {
+                multipart.addBodyPart(messageBodyPartForText);
+                message.setContent(multipart);
+            }
+
             Transport.send(message);
             System.out.println("Mail successfully sent");
-        }
-        catch (MessagingException mex)
-        {
-            mex.printStackTrace();
+        } catch (MessagingException mex) {
+            throw new CustomAppException("Could not send Email", "SERVER.EAMIL_UTILITY.COULD_NOT_SEND_EMAIL", HttpStatus.EXPECTATION_FAILED);
         }
     }
 }
