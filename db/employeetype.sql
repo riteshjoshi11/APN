6c9830aa024506058d03104072e5778ab223bb40
Nitesh Yadav <nitesh.yadav@idmworks.com>
2020-07-16 23:27:52 +0530

    Updated some values in the employeetype.sql

--- db/employeetype.sql
+++ db/employeetype.sql
@@ -1,4 +1,5 @@
 use `antrackerdb`;
 SET SQL_SAFE_UPDATES = 0;
 delete from employeetype;
-INSERT INTO employeetype VALUES (1,'SUPER_ADMIN'),(2,'partner'),(3,'sales'),(4,'labour'),(5,'accountant'),(6,'VIRTUAL'),(7,'default_employee');
+INSERT INTO employeetype VALUES (1,'SUPER_ADMIN'),(2,'Business Partner'),(3,'Sales Person'),(4,'Labour'),(5,'Accountant'),(6,'VIRTUAL'),(7,'Default Employee');
+
