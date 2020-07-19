b337e1aaee8d0dfa7d09ebbd11be24c71d93b87c
Paras Kavdikar <paraskavdikar@gmail.com>
2020-07-19 09:14:47 +0530

    Patch for phonebook created.

---
+++ db/Patch_19July.sql
@@ -0,0 +1,20 @@
+use antrackerdb;
+drop table if exists `userphonebook`;
+drop table if exists `userdetail`;
+CREATE TABLE `userdetail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `userphonebook` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userdetailid` int(11) DEFAULT NULL,
+  `savedmobile1` varchar(15) DEFAULT NULL,
+  `savedmobile2` varchar(15) DEFAULT NULL,
+  `savedemail` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userphonebook_ibfk_1_idx` (`userdetailid`),
+  CONSTRAINT `userphonebook_ibfk_1` FOREIGN KEY (`userdetailid`) REFERENCES `userdetail` (`id`)
+);
