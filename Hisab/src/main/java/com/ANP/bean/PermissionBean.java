56109d49536617ddfc161958448bbf5650dde6c5
Ritesh Joshi <riteshjoshi11@gmail.com>
2020-08-29 23:11:20 +0530

    updated version of spring boot to 2.3.1

--- Hisab/src/main/java/com/ANP/bean/PermissionBean.java
+++ Hisab/src/main/java/com/ANP/bean/PermissionBean.java
@@ -1,4 +1,21 @@
 package com.ANP.bean;
 
+import java.util.Map;
+
 public class PermissionBean {
+    boolean isShowEmployeePersonalbalance;
+    boolean isShowSalaryBal;
+    boolean isshowDashBoard;
+    boolean isEmployeeAccountView;
+
+
+    private Map<String, Boolean> permission;
+    String permissionname;
+    boolean ispermission;
+
+
+    public boolean canshowCustimerbal() {
+        return permission.get("customerbalanceview");
+    }
+
 }
