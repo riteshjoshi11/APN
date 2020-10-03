40a239cb5a9a300a8b0aab5df58194e603edb892
Nitesh Yadav <nitesh.yadav@idmworks.com>
2020-10-04 00:36:36 +0530

    Added logger in the permission related artifact

--- Hisab/src/main/java/com/ANP/service/RoleTypeBeanSingleton.java
+++ Hisab/src/main/java/com/ANP/service/RoleTypeBeanSingleton.java
@@ -2,6 +2,8 @@ package com.ANP.service;
 
 import com.ANP.bean.PermissionBean;
 import com.ANP.repository.SystemConfigurationReaderDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,17 +11,21 @@ import java.util.Map;
 
 @Service
 public class RoleTypeBeanSingleton {
-
     @Autowired
     SystemConfigurationReaderDAO systemConfigurationReaderDAO;
 
+    private static final Logger logger = LoggerFactory.getLogger(RoleTypeBeanSingleton.class);
     private Map<Integer, PermissionBean> roleTypeBeanMap = null;
 
     public PermissionBean getPermissionBean(Integer employeeType) {
         if (roleTypeBeanMap == null) {
+            logger.info("Initializing the Permission Data...");
             roleTypeBeanMap = systemConfigurationReaderDAO.getPermissionBeanMap();
+            logger.info("Initialization complete" + roleTypeBeanMap);
         }
-        return this.roleTypeBeanMap.get(employeeType);
+        PermissionBean permissionBean = this.roleTypeBeanMap.get(employeeType);
+        logger.trace("Initialization complete" + permissionBean);
+        return  permissionBean;
     }
 
 }
